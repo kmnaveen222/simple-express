bf5e665ff0562b8901150ee823a7baca00de9009
kmnaveen222 <kmnaveen222@gmail.com>
2024-03-13 16:15:21 +0530

    simple

---
+++ simple-express.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.listen(process.env.PORT || 3000, ()=> {
+console. log( `listening on port ${process.env.PORT || 3000} `);});
+
+app.get('/', async (req, res)=> {
+res . send( "<h1>World !</h1>");});
